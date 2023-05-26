966334c9d3a44e8b26127b4455f5aa15e8176575
AMERICAS\paridaa <ASHUTOSH.PARIDA@HPE.COM>
2023-05-26 18:53:06 -0400

    - Add final methods

--- app/src/main/java/sample/java/app/App.java
+++ app/src/main/java/sample/java/app/App.java
@@ -4,19 +4,24 @@
 package sample.java.app;
 
 public class App {
-    public int add(int a, int b) {
+
+    //Addition
+    public final int add(int a, int b) {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    //Subtraction
+    public final int subtract(int a, int b) {
         return a - b;
     }
 
-    public int multiply(int a, int b) {
+    //Multiplication
+    public final int multiply(int a, int b) {
         return a * b;
     }
 
-    public int divide(int a, int b) {
+    //Division
+    public final int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
         }
